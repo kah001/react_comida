ee8f25bde2c298673cfaf23e09e3bd1ca4694034
Jennifer Santos de Lima <ra54310463819@cursos.insf>
2024-10-07 16:37:23 -0300

    adicionar o hook useNavigate nas paginas

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,23 @@
 import './index.scss'
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Home() {
+    const [nome, setNome] = useState('')
+    const [senha, setSenha] = useState('')
+
     return (
         <div className='pagina-inicio'>
+            <div className='form'>
+                <h1>Usuário</h1>
 
-            <Link to='/salvar'>Salvar</Link>
-            <Link to='/consultar'>Consultar</Link>
-            <Link to='/deletar'>Deletar</Link>
+                <label>Nome:</label>
+                <input type="text" value={nome} onChange={a => setNome(a.target.value)} />
 
+                <label>Senha:</label>
+                <input type="text" placeholder='********' value={senha} onChange={a => setSenha(a.target.value)} />
+
+                <div className='botao'>ENTRAR</div>
+            </div>
         </div>
     )
 }
